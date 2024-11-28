911354f164189b1c537f61ffb8e0b0f8a2f81086
RalphHellisng62 <arenasrafael889@gmail.com>
2024-11-27 18:01:54 -0600

    ciclo1.py

--- bucle1.py
+++ bucle1.py
@@ -6,10 +6,10 @@ Por ejemplo 5! = 1x2x3x4x5=120)
 '''
 print("Calcular Factorial ")
 
-n = int(input("Escribe un numero: "))
+num = int(input("Escribe un numero: "))
 fact = 1
 for i in range (1,n + 1):
     fact *= i 
     
     
-print(f"El factorial de {n} es {fact}")
+print(f"El factorial de {num} es {fact}")
